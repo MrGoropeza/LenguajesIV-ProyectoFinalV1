5eae9dd18a1fa4436ba4f2fa80794ec53e401c27
francoCAVS <francocavs02@gmail.com>
2021-11-13 18:38:43 -0300

    I

---
+++ ProyectoFinalV1/Views/InAppPages/AmigosPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProyectoFinalV1.Views.InAppPages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AmigosPage : ContentPage
+    {
+        public AmigosPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
